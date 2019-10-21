7a8d415fad0d58f803b7e4a5baf87bdadbf604c3
LiChengEn <leslie.li@dsp.im>
2019-10-21 17:18:28 +0800

    #10

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': os.path.join(BASE_DIR, 'bnb_paris'),
+        'NAME': os.path.join(BASE_DIR, 'bnb_paris.sqlite3'),
     }
 }
 
--- backend/bnb/views.py
+++ backend/bnb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from bnb.models import BnbParis
+import csv
 
 
 def hello_world(request):
@@ -10,4 +12,26 @@ def hello_world(request):
 
 
 def index(request):
+    with open("csv/demo2.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            _, created = BnbParis.objects.get_or_create(
+                bnb_id=row[0],
+                name=row[1],
+                host_id=row[2],
+                host_name=row[3],
+                neighbourhood_group=row[4],
+                neighbourhood=row[5],
+                latitude=row[6],
+                longitude=row[7],
+                room_type=row[8],
+                price=row[9],
+                minimum_nights=row[10],
+                number_of_reviews=row[11],
+                last_review=row[12],
+                reviews_per_month=row[13],
+                calculated_host_listings_count=row[14],
+                availability_365=row[15],
+            )
+    print("ok!")
     return render(request, 'index.html')
